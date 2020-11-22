25facb0f0aaf14ce8fcc9d4011af9275c9fd9fa3
mamun6 <74857061+mamun6@users.noreply.github.com>
2020-11-22 19:30:08 +0600

    Add files via upload
    html test

---
+++ file.js
@@ -0,0 +1,16 @@
+var marks = promt("Enter your number:");
+
+if(marks>=80 && marks<=100)
+console.log("A+");
+ealseif (marks>=70 && marks<=79)
+console.log("A");
+ealseif(marks>=60 && marks<=69)
+console.log("A-");
+ealseif(marks>=50 && marks<=59)
+console.log("B");
+ealseif(marks>=40 && marks<=49)
+console.log("c")
+ealseif(marks>=33 && marks<=39)
+console.log("D");
+
+else("Fail")
